b2e507d1a03f9a5757c5e160aaca29a950a15b0d
wangbinxiang <45696104@qq.com>
2016-09-06 12:13:32 +0800

    config修改

---
+++ server-src/config/apiFeatureConf.js
@@ -0,0 +1,5 @@
+export const userGet = Symbol();
+export const userLoginIn = Symbol();
+export const userSignUp = Symbol();
+export const userUpdatePassword = Symbol();
+export const userRestPassword = Symbol();
