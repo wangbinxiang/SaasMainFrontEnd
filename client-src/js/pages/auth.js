ea779d714259b324d80f5b71b7b08cfad27c7555
Trauquen YEEN <shiver@shiver.cn>
2016-09-18 14:15:30 +0800

    Change register.ejs.

--- client-src/js/pages/auth.js
+++ client-src/js/pages/auth.js
@@ -4,6 +4,35 @@ if (module.hot) {
 
 import 'foundation-sites'
 $(document).foundation();
+Foundation.Abide.defaults.patterns['psw_complexity'] = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/;
+// ^                         Start anchor
+// (?=.*[A-Z].*[A-Z])        Ensure string has two uppercase letters.
+// (?=.*[!@#$&*])            Ensure string has one special case letter.
+// (?=.*[0-9].*[0-9])        Ensure string has two digits.
+// (?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+// .{8}                      Ensure string is of length 8.
+// $                         End anchor.
+
+$('#formReg').on('click', () => {
+    $('#formReg').foundation('validateForm');
+    if($('#codeInput').is('[data-invalid]')){    
+        $('#formReg').foundation('addErrorClasses', $('#codeInput'));
+    }
+    if($('[data-invalid]').length > 0){
+        fetch('https://davidwalsh.name/submit-json', {
+            method: 'post',
+            body: JSON.stringify({
+                mobil: $('[name="cellPhone"]').val(),
+                password: $('[name="password"]').val(),
+                password: $('[name="code"]').val()
+            })
+        });
+    } else{
+        return false;
+    }
+    return false;
+})
+
 require.ensure([], function(require) {
 
 })
